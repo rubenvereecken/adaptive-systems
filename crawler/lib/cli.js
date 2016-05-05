6ae8d391608d8c2aca955ca87d6101b891dc7d1b
Ruben Vereecken <rubenvereecken@gmail.com>
2016-05-05 19:33:16 +0200

    fix for exclude

--- crawler/lib/cli.js
+++ crawler/lib/cli.js
@@ -16,7 +16,7 @@ class CLI {
     const url = args.args[1];
     if (!args.exclude)
       args.exclude = []
-    else (args.exclude.match(/,/))
+    else if (args.exclude.match(/,/))
       args.exclude = args.exclude.split(',')
     var options = {
       overwrite: args.overwrite || false,
